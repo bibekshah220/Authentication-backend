6ddf8128ccb01926cf0e2d1eb5bec24db451b539
bibekshah220 <bibekshah425@gmail.com>
2026-03-21 14:46:42 +0545

    improve error message for user registration validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,10 +6,12 @@ export const registerUser = TryCatch(async(req, res) => {
 
     const sanitizedData = sanitize(req.body);
     const validation = registerSchema.safeParse(sanitizedbody);
+
+    const zodError = validation.error;
  
     if(!validation.success) {
         return res.status(400).json({
-            message: "Validation failed",
+            message: zodError,
         });
     }
 
