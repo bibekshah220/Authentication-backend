5e130e1b3d89b1a3ca53b82d5f3419862c8f0740
bibekshah220 <bibekshah425@gmail.com>
2026-03-22 22:17:30 +0545

    add verifyRefreshToken function to handle refresh token validation

--- config/generateToken.js
+++ config/generateToken.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/index.js";
+import { ca } from "zod/v4/locales";
+import { redisClient } from "../server.js";
 
 
 
@@ -31,3 +33,16 @@ res.cookie("refreshToken", refreshToken, {
 
 return { accessToken, refreshToken };
 }
+
+export const verifyRefreshToken = async (refreshToken) => {
+    try {
+const decode = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+const storedToken = await redisClient.get(`refresh_Token:${decode.id}`);
+if (storedToken === refreshToken) {
+   return decode;
+}
+return null;
+
+}catch (error) {
+return null;
+}}
